c687de3169a0a07dd86318bbc1317495dbc55ed9
ESTEBAN <esteban.v.web@gmail.com>
2021-11-02 19:34:50 -0500

    add EB api host

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagramapinode12-env.eba-pbjyxrkj.us-east-1.elasticbeanstalk.com",
 };
 
 /*
